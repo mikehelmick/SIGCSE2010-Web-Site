b115b6a17d8f60a41ec27d833f9b664f2b3c85d8
Mike Helmick <mike.helmick@gmail.com>
2009-06-09 20:30:27 -0700

    issue #2 - add new CFP to Web site

--- presenters/index.php
+++ presenters/index.php
@@ -19,34 +19,39 @@ The 41st ACM Technical Symposium on Computer Science Education</h2>
 <h3>March 10-13, 2010, Milwaukee, WI, USA<br/>
 <a href="http://www.sigcse.org/sigcse2010/">http://www.sigcse.org/sigcse2010/</a></h3>
  
-<p><b>CONFERENCE THEME: Making Contact </b><br/>
-SIGCSE 2010 continues the long tradition of bringing together colleagues from around the world to make contact via paper, panel, poster and special sessions, as well as workshops, birds-of-a-feather, and informal settings at breaks and meals. We celebrate and encourage these contacts that allow us to renew and make new connections as we discuss the challenges and excitement of computer science education.</p>
  
+ 
+<p><b>CONFERENCE THEME: Making Contact</b><br />
+SIGCSE 2010 continues the long tradition of bringing together colleagues from around the world to make contact via paper, panel, poster and special sessions, as well as workshops, birds-of-a-feather, and informal settings at breaks and meals. We celebrate and encourage these contacts that allow us to renew and make new connections as we discuss the challenges and excitement of computer science education.</p>
+
 <p>The SIGCSE Technical Symposium addresses problems common among educators working to develop, implement and/or evaluate computing programs, curricula, and courses. The symposium provides a forum for sharing new ideas for syllabi, laboratories, and other elements of teaching and pedagogy, at all levels of instruction. We invite those interested in computer science education and computer science education research to contribute to SIGCSE 2010.</p>
-   
+
 <p>Following SIGCSE tradition, the symposium will provide a diverse selection of technical sessions and opportunities for learning and interaction.</p>
  
-<p><b>PAPERS</b><br/>
-Papers describe a classroom experience, teaching technique, curricular initiative, or educational research project. Two versions of a submission are required: a full version having author names and affiliations and a version for use in reviewing having a level of anonymity chosen by the author, ranging from a completely anonymous version having no references to the authors or affiliations to a duplicate of the full version.  Papers will undergo a blind reviewing process and must not exceed five pages. Authors will have approximately 25 minutes for their presentations, including questions and answers.  At least one author of each accepted paper must register and attend the conference to present. </p>
+<p><b>PAPERS</b><br />
+Papers describe a classroom experience, teaching technique, curricular initiative, or educational research project. Two versions of a submission are required: a full version having author names and affiliations and an anonymous version for use in reviewing. The anonymous version must not include author names, affiliations, or obvious references to the authors or their institutions. Papers will undergo a double-blind reviewing process and must not exceed five pages. Authors will have approximately 25 minutes for their presentations, including questions and answers.  At least one author of each accepted paper must register and attend the conference to present.</p>
  
-<p><b>PANELS</b><br/> 
-Panels present multiple perspectives on a specific topic. To allow each panelist sufficient time to present his or her perspective and still enable audience participation, a panel will normally have at most four panelists, including one moderator. Panel submissions should include a list of the panelists, their affiliations, and a description of the topic, with brief position statements from panelists. Proposals with more than four panelists must provide a statement connecting the extra panelist to the effectiveness of the panel and must convincingly show that each panelist will be able to speak, and the audience able to respond, within the session time. Panel abstracts must not exceed two pages. A panel session is approximately 75 minutes long.</p>
+<p><b>PANELS</b><br />
+Panels present multiple perspectives on a specific topic. To allow each panelist sufficient time to present his or her perspective and still enable audience participation, a panel will normally have at most four panelists, including one moderator. Panel submissions should include a list of the panelists, their affiliations, and a description of the topic, with brief position statements from panelists. Proposals with more than four panelists must provide a statement connecting the extra panelist(s) to the effectiveness of the panel and must convincingly show that each panelist will be able to speak, and the audience able to respond, within the session time. Panel abstracts must not exceed two pages. A panel session is approximately 75 minutes long.</p>
  
-<p><b>SPECIAL SESSIONS</b><br/>
-Special sessions are your opportunity to customize and experiment with the SIGCSE conference format. Possible special sessions include a seminar on a new topic, a committee report, or a forum on curriculum issues. More generally, they must be 75 minutes in length, held in standard conference spaces, and justifiably distinct from the panel, paper, and poster tracks. Within those constraints, the form is yours to design. Special session abstracts must not exceed two pages.</p>
+<p><b>SPECIAL SESSIONS</b><br />
+Special sessions are your opportunity to customize and experiment with the SIGCSE conference format. Possible special sessions include a seminar on a new topic, a committee report, or a forum on curriculum issues. More generally, they must be 75 minutes in length, held in standard conference spaces, and justifiably distinct from the panel, paper, and poster tracks. Within those constraints, the form is yours to design. Special session abstracts must not exceed two pages.</p> 
  
-<p><b>WORKSHOPS</b><br/>  
+<p><b>WORKSHOPS</b><br />
 Workshops provide introductory and advanced topics to help make participants more effective teachers and scholars. Workshop proposals (including abstract) must not exceed two pages. Submissions must specify equipment needs (e.g., participant laptops or projector required) and any limitation on the number of participants. Workshops are scheduled for a three-hour session and are offered separately from the technical track session times.</p>
  
-<p><b>BIRDS OF A FEATHER SESSIONS</b><br/>  
-Birds of a Feather (BOF) sessions provide an environment for colleagues with similar interests to meet for informal discussions. A maximum one-page description (including abstract) is requested to describe the informal discussion topic. A/V equipment will not be provided for these sessions. Approximately 45 minutes are allocated to each BOF topic.</p> 
+<p><b>BIRDS OF A FEATHER (BOF) SESSIONS</b><br />
+Birds of a Feather sessions provide an environment for colleagues with similar interests to meet for informal discussions. A maximum one-page description (including abstract) is requested to describe the informal discussion topic. A/V equipment will not be provided for these sessions. Approximately 45 minutes are allocated to each BOF topic. Submit for a regular BOF slot by November 2. 
  
-<p><b>POSTERS</b><br/>
-Posters describe CS Education materials or research, particularly works in progress. Proposals (including abstract) are limited to two pages. Poster demonstrations are scheduled to permit one-on-one discussion with conference attendees, typically during session breaks. Prepared handouts are encouraged in order to share your work.</p>
+<p><b><font color="red">NEW THIS YEAR:</font> LIGHTNING BOFs</b><br />
+Lightning BOFs are a small number of BOF slots that are reserved for "hot" topics that emerge as the conference approaches. Submit for a Lightning BOF between December 15 and January 15. A BOF that is not accepted for a regular slot may be resubmitted as a Lightning BOF.</p>
  
-<p><b>VIDEO EXHIBITION</b><br/> 
-SIGCSE 2010 will continue the video exhibition begun at SIGCSE 2009, but with a focus aimed at facilitating the conference theme.  We are interested in video submissions that demonstrate real or dramatized challenging pedagogical situations or challenges for computer science education research. The videos should be capable of sparking discussion rather than demonstrating a proposed solution.</p>
-
+<p><b>POSTERS</b><br />
+Posters describe computer science education materials or research, particularly works in progress. Proposals (including abstract) are limited to two pages. Poster demonstrations are scheduled to permit one-on-one discussion with conference attendees, typically during session breaks. Prepared handouts are encouraged in order to share your work.</p> 
+ 
+<p><b>VIDEO EXHIBITION</b><br />
+SIGCSE 2010 will continue the video exhibition begun at SIGCSE 2009. While any relevant high-quality video will be considered for exhibition, videos that highlight the conference theme are preferred. To that end, we are interested in videos that demonstrate real or dramatized challenging pedagogical situations or computing education research challenges. These videos should be capable of sparking discussion rather than demonstrating a proposed solution.</p>
+ 
 <p><b>IMPORTANT DUE DATES</b><br/>
 Friday, September 11, 2009: Paper, Panel, Special Session, and Workshop submissions<br/>
 Monday, November 2, 2009: Birds of a Feather, Poster, and Video submissions<br />
