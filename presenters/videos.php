b8902835588a1ee5c7ea2b16b9dbf490002cf8b7
Mike Helmick <mike.helmick@gmail.com>
2009-06-21 10:33:43 -0700

    comments from dave

--- presenters/videos.php
+++ presenters/videos.php
@@ -18,11 +18,13 @@
   <td width="50%" align="left" valign="top">
   <h3>Contents:</h3>
   <ul>
-    <li><a href="#what">What Is The Channel SIGCSE Video Exhibition?</a></li>
-    <li><a href="#kinds">What Kind Of Videos Can I Submit?</a></li>
-    <li><a href="#submit">How Do I Submit A Video For Exhibition?</a></li>
-    <li><a href="#selected">What If My Video Is Selected For Exhibition?</a></li>
-    <li><a href="#propose">Can I Propose A Video That I Didn't Create?</a></li>
+    <li><a href="#whatis">What is The Channel SIGCSE Video Exhibition?</a></li>
+    <li><a href="#what">What will be on Chanel SIGCSE?</a></li>
+    <li><a href="#where">Where will people see my video?</a></li>
+    <li><a href="#kind">What kind of videos can I submit?</a></li>
+    <li><a href="#how">How do I submit a vide for exhibition?</a></li>
+    <li><a href="#if">What If My Video Is Selected For Exhibition?</a></li>
+    <li><a href="#notowner">Can I proposa a video taht I didn't create?</a></li>
   </ul>
   
   </td>
@@ -52,195 +54,84 @@
            
            
 <div class="SectionHeader"><div class="Full">
-<h2><a name="what">What Is The Channel SIGCSE Video Exhibition?</a></h2>
+<h2><a name="whatis">What Is The Channel SIGCSE Video Exhibition?</a></h2>
 </div></div>
 
-            <p>
-            What happens each day at SIGCSE after the technical sessions
-            are done?  In the past, attendees might have undertaken mundane
-            activities such as drinking and dining.  In Chattanooga,
-            instead of gathering in restaurants and bars to discuss
-            the day’s technical sessions, SIGCSE attendees will be
-            gathering in hotel rooms, ordering room service, and watching
-            Channel SIGCSE! (It could happen.)
-            </p>
-
-            <p>
-            What will be on Channel SIGCSE you ask?  You, your students,
-            and your animations in the videos you submitted to the
-            Channel SIGCSE video exhibition.  Here’s your chance to share
-            the dynamic material you haven’t had a good way
-            to share - until now. 
-            </p>
-
-            <p>
-            Where will people see my video?  On Channel SIGCSE, of course.
-            Channel SIGCSE will be presented on the closed-circuit video
-            of the Chattanooga conference center and conference hotels
-            during SIGCSE 2010 in Chattanooga. It is possible the videos
-            will be available to attendees on DVD – we’re still working
-            out the details.
-            </p>
-
-            <p>
-            Videos, old and new, are accepted for Channel SIGCSE exhibition.
-            For more information on submissions, keep reading.
-            </p>
+<p>Channel SIGCSE is the Video Exhibition at the SIGCSE Symposium. Videos submitted by you, and your SIGCSE colleagues are peer-reviewed to be accepted for exhibition. The Video Exhibition appeared at SIGCSE for the first time in 2009 at Chattanooga.</p>
 
 
 <div class="SectionHeader"><div class="Full">
-<h2><a name="kinds">What Kind Of Videos Can I Submit?</a></h2>
+<h2><a name="what">What will be on Chanel SIGCSE?</a></h2>
 </div></div>
 
-
-            <p>
-            We invite videos relevant to computer science education.
-            These videos could include classroom lectures, demonstrations,
-            or animations.  The videos could be useful in today’s
-            classroom or be of historical significance. 
-            </p>
-
-            <p>
-            To be accepted, a video must be:
-            </p>
-
-            <ul>
-              <li> relevant to computer science educators
-                   </li>
-              <li> of reasonable technical quality<sup>*</sup>
-                   and running time<sup>**</sup>
-                   </li>
-              <li> submitted with a permission form from the copyright
-                   owner (see permission form below)
-                   </li>
-            </ul>
-
-            <br />
-
-            <p>
-            <sup>*</sup> Reasonable quality:  There are many aspects of
-            reasonable quality, and we won’t attempt to list them here;
-            however, the panel will know reasonable quality when they
-            see it.  We’re not SIGGRAPH and this isn’t Sundance, so flaws
-            are accepted (if not expected).
-            </p>
-
-            <p>
-            <sup>**</sup> Reasonable runtime:  While the running time of
-            a video will be a factor in the selection process, there is
-            no hard limit for the running time of a video submission.
-            As with any video production, you should strive for maximum
-            impact in the least amount of time necessary.
-            </p>
+<p>You, your students, and your animations in the videos you submitted to the Channel SIGCSE video exhibition. Here’s your chance to share the dynamic material you haven’t had a good way to share - until now.</p>
 
 <div class="SectionHeader"><div class="Full">
-<h2><a name="submit">How Do I Submit A Video For Exhibition?</a></h2>
+<h2><a name="where">Where will people see my video?</a></h2>
 </div></div>
 
-           <ol>
-              <li> <strong>Create a video of interest to the SIGCSE
-                   community</strong>.
-                   </li>
-              <li> <strong>Create and post a web-version of the video</strong>.
-                   Post the web-version of your video to
-                   <a href="http://youtube.com">YouTube.com</a>.
-                   The review panel will view your YouTube video.
-                   If the video is accepted for publication a high-quality
-                   version will be required.
-                   </li>
-              <li> <strong>Select, or create, a representative image</strong>.
-                   The representative images will be used in promotion and,
-                   possibly, on the DVD of exhibition videos.  The
-                   representative image is typically a frame of the video.
-                   It could be the title image, or a particularly memorable
-                   image from the video. The representative image must be
-                   included in the submission form, so you need the image
-                   before completing the submission form.
-                   </li>
-              <li> <strong>Complete a video proposal submission form</strong>.
-                   The submitter will complete the submission form
-                   (
-                   <a href="/sigcse2010/downloads/SIGCSE2010_VideoSubmissionForm.doc">Word</a>,
-                   <a href="/sigcse2010/downloads/SIGCSE2010_VideoSubmissionForm.txt" onclick="target='newwindow'">Text</a>
-                   or
-                   <a href="/sigcse2010/downloads/SIGCSE2010_VideoSubmissionForm.rtf">RTF</a>
-                   )
-                   and email the completed form to <script>write_email( "dbouvie", "siue.edu" );</script>.
-                   </li>
-
-            </ol>
-
-            <p>
-            <strong><i>Deadline:</i></strong>
-            All electronic submissions must be <strong>received</strong>
-            by <strong class="warning">November 2, 2009</strong>.
-            Notifications will be sent by tbd.
-            </p>
-
+<p>On Channel SIGCSE, of course! Channel SIGCSE will be presented on video kiosks at the symposium during SIGCSE 2010. Channel SIGCSE also has a home on the web throughout the year</p>
 
 <div class="SectionHeader"><div class="Full">
-<h2><a name="selected">What If My Video Is Selected For Exhibition?</a></h2>
+<h2><a name="kind">What kind of videos can I submit?</a></h2>
 </div></div>
 
-         <p>
-            All submitted videos will be panel 'reviewed'.  There isn't
-            much we can tell you about the review process except that
-            we will notify submitters by November 22, 2008 if their video
-            was selected for exhibition.  If you submitted a video selected
-            for exhibition, there are a few more things to do:
-            </p>
+<p>We invite videos relevant to computer science education. These videos could include classroom lectures, demonstrations, or animations. The videos could be useful in today’s classroom or be of historical significance. For SIGCSE 2010 we prefer videos that highlight the conference theme. To that end, we are interested in videos that demonstrate real or dramatized challenging pedagogical situations or computing education research challenges. These videos should be capable of sparking discussion rather than demonstrating a proposed solution.</p>
 
-            <ol>
-              <li> We’ll need your permission to exhibit the video.
-                   Download and complete the permission form
-                   (
-                   <a href="/sigcse2010/downloads/SIGCSE2010_VideoPermissionForm.doc">Word</a>
-                   or
-                   <a href="../downloads/SIGCSE2010_VideoPermissionForm.pdf"  onclick="target='newwindow'">PDF</a>
-                   ).
-                   We need a signature, so mail
-                   (snail-mail) the completed and signed form to:
+<p>To be accepted, a video must be:<br />
+<ul>
+  <li>relevant to computer science educators</li>
+  <li>of reasonable technical quality (<strong>1</strong>) and running time (<strong>2</strong>)</li>
+  <li>submitted with a permission form from the copyright owner (see permission form below)</li>
+</ul>
+</p>
 
-                   <br />
-                   <br />
-
-                   <p>
-                   Dennis Bouvier
-                   <br />
-                   SIUE Computer Science
-                   <br />
-                   Box 1656
-                   <br />
-                   Edwardsville, IL 62026 USA
-                   </p>
-                   </li>
-              <li> Send us the high-quality version of the video.
-                   Email, snail-mail or send a link; whatever works for
-                   you will work for us.
-                   </li>
-            </ol>
-
-            <p>
-            <strong><i>Deadlines:</i></strong>
-            High-quality video must be <strong>submitted</strong>
-            by <strong class="warning">tbd</strong>.
-            All permission forms must be <strong>postmarked</strong>
-            by <strong class="warning">tbd</strong>.
-            SIGCSE is not responsible for permission forms lost
-            in the mail.
-            </p>
+<ol>
+  <li>Reasonable quality: There are many aspects of reasonable quality, and we won’t attempt to list them here; however, the panel will know reasonable quality when they see it. We’re not SIGGRAPH and this isn’t Sundance, so flaws are accepted (if not expected).</li>
+  <li>Reasonable runtime: While the running time of a video will be a factor in the selection process, there is no hard limit for the running time of a video submission. As with any video production, you should strive for maximum impact in the least amount of time necessary.</li>
+</ol>
 
 <div class="SectionHeader"><div class="Full">
-<h2><a name="propose">Can I Propose A Video That I Didn't Create?</a></h2>
+<h2><a name="how">How do I submit a vide for exhibition?</a></h2>
 </div></div>
-         
-            <p>
-            No, you must be the rights-owner of the submission to complete
-            the permission form and therefore submit a video to the
-            exhibition.  But, you can encourage the rights owner to make
-            the submission.
-            </p>         
-         
+
+<p><ol>
+  <li>Create a video of interest to the SIGCSE community.</li>
+  <li>Create and post a web-version of the video. Post the web-version of your video to YouTube.com. The review panel will view your YouTube video. If the video is accepted for publication a high-quality version will be required.</li>
+  <li>Select, or create, a representative image. The representative images will be used in promotion and, possibly, on the DVD of exhibition videos. The representative image is typically a frame of the video. It could be the title image, or a particularly memorable image from the video. The representative image must be included in the submission form, so you need the image before completing the submission form.</li>
+  <li>Complete a video proposal submission form. The submitter will complete the submission form (Word, Text or RTF) and email the completed form to <script>write_email( "sigcse10-videos", "cs.xu.edu" );</script>.</li>
+</ol></p>
+
+<p><strong>Deadlines</strong>: dates are posted at the top of the page.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="if">What If My Video Is Selected For Exhibition?</a></h2>
+</div></div>
+
+<p>All submitted videos will be panel-reviewed. For the videos selected for exhibition, the submitters are required to:
+
+<ol>
+  <li>Download and complete the permission form (Word or PDF). We need a signature, so mail (snail-mail) the completed and signed form to:
+  <pre>
+      Dennis Bouvier
+      SIUE Computer Science
+      Box 1656
+      Edwardsville, IL 62026 USA
+  </pre>
+  </li>
+  <li>Send us the high-quality version of the video. Snail-mail a (NTSC) DVD disk or mini-DV tape, or send a link to a file (of just about any format); in general, whatever works for you will work for us.</li>
+  </ol>
+</p>
+
+<p><b>Deadlines:</b> High-quality video must be submitted by the date listed above. All permission forms must be postmarked by the high-quality submission date. SIGCSE is not responsible for permission forms lost in the mail.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="notowner">Can I proposa a video taht I didn't create?</a></h2>
+</div></div>
+
+
+<p>No, you must be the rights-owner of the submission to complete the permission form and therefore submit a video to the exhibition. But, you can encourage the rights owner to make the submission.</p>
+
 
 <div class="SectionHeader"><div class="Full">
 <h2><a name="presentation">Questions</a></h2>
