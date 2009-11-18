97eee09e5ba91340892dfe5ff4c5af41fc6caa4f
Mike Helmick <mike.helmick@gmail.com>
2009-11-17 22:20:48 -0800

    added hotel and travel information

--- attendees/accommodations.php
+++ attendees/accommodations.php
@@ -13,8 +13,61 @@
 	         <!-- right area -->
              
 <h1>SIGCSE 2010 Accommodations</h1>
-      
-<div class="errorExplanation">Information is coming soon!</div>  
+
+<p>To help conference attendees begin their travel planning, we are happy to provide the following information about the hotels which will have blocks of rooms available at special conference rates. And, as SIGCSE has done in the past, we have a roommate matching website to help pair you with a roommate to reduce the cost of attending the symposium.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2>Hotels</h2>
+</div></div>
+
+<p>There are two conference Hotels, the Hyatt Regency and the Hilton City Center;<br/>
+<ul>
+  <li>both are connected to the convention center by a heated skywalk</li>
+  <li>Both cost $145 per night</li>
+  <li>Both have free wireless Internet</li>
+</ul>
+</p>
+
+<table class="fancy">
+ <tr>
+  <td align="left" valign="top" nowrap="nowrap">
+<p><b>Hyatt Regency Information</b></p>
+
+<p><a href="https://resweb.passkey.com/go/SIGCSE2010">Online Reservations</a><br/>
+Reservations by phone:<br/>
+1-888-421-1442 : Group code is G-CACM</p>
+
+<ul>
+ <li>Check-in at 3 pm</li>
+ <li>Check-out at noon</li>
+</ul>
+  </td>
+  <td>
+<p><b>Hilton City Center Information</b></p>
+
+<p>Online Reservations Coming Soon<br/>
+Reservations by phone:<br/>
+1-414-271-7250 : Group Name: SIGCSE 2010</p>
+
+<ul>
+  <li>Free Parking</li>
+  <li>Check-in at 4 pm</li>
+  <li>Check-out at 11 am</li>
+</ul>
+  </td>
+ </tr>
+</table>
+
+
+
+
+<br/>
+<div class="SectionHeader"><div class="Full">
+<h2>Roommate Matching</h2>
+</div></div>
+<p>We recognize that many SIGCSE attendees work at schools with very limited -- even nonexistent! -- travel budgets.  To help you reduce your cost to participate in the symposium, we are happy to once again provide assistance with roommate matching: <a href="http://www.cs.rit.edu/sigcse/">The SIGCSE 2009 Roommate Database</a>. Once registered, you can input your preferences and see other registrants who have compatible preferences.  The rest is up to you!</p>
+
+<p>The roommate database is hosted by the <a href="http://www.cs.rit.edu/">Rochester Institute of Technology Department of Computer Science</a> and is administered by <a href="http://www.cs.rit.edu/~ptt/">Paul Tymann</a>. Please <a href="mailto:ptt@cs.rit.edu">contact Paul</a> with any questions that you may have about this service.</p>
 
 
 
--- attendees/travel_information.php
+++ attendees/travel_information.php
@@ -14,9 +14,48 @@
              
 <h1>SIGCSE 2010 Travel Information</h1>
       
-<div class="errorExplanation">Information is coming soon!</div>  
+<div class="errorExplanation">This information is still being updated, check back soon!</div>  
 
+<h3>Contents:</h3>
+  <ul>
+    <li><a href="#airline">Airline Service</a></li>
+    <li><a href="#shuttle">Go Airport Shuttles</a></li>
+    <li><a href="#taxis">Taxis and Hotel Shuttles</a></li>
+    <li><a href="#rentals">Car Rentals</a></li>
+    <li><a href="#parking">Daily Parking</a></li>
+  </ul>
 
+<div class="SectionHeader"><div class="Full">
+<h2><a name="airline">&nbsp;</a>Airline Service</h2>
+</div></div>
+
+<p>Commercial airline service to Milwaukee is handled by the <a href="http://www.mitchellairport.com/">General Mitchell International Airport</a> (Code: MKE). Reserving a flight directly to Milwaukee will provide the most convenient access to the conference.  Please visit the airport's web site for the current list of airlines that serve Milwaukee.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="shuttle">&nbsp;</a>Go Airport Shuttles</h2>
+</div></div>
+
+<p>We have negotiated reduced costs for transportation to/from the airport from Go Airport Connection.  Attendees can reserve a round trip fare for $20 by booking at least 48 hours in advance
+through the customized portal at <a href="http://goairportconnection.hudsonltd.net/res?USERIDENTRY=sigcse10&LOGON=GO">http://goairportconnection.hudsonltd.net/res?USERIDENTRY=sigcse10&amp;LOGON=GO</a>
+</p>
+
+<p>Attendees can also access this through the Go website (<a href="http://www.goairportconnection.com/">http://www.goairportconnection.com/</a>) and type in the code SIGCSE10 that will
+take them to the customized portal.</p>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="taxis">&nbsp;</a>Taxis and Hotel Shuttles</h2>
+</div></div>
+<div class="errorExplanation">Information coming soon.</div>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="rentals">&nbsp;</a>Car Rentals</h2>
+</div></div>
+<div class="errorExplanation">Information coming soon.</div>
+
+<div class="SectionHeader"><div class="Full">
+<h2><a name="parking">&nbsp;</a>Daily Parking</h2>
+</div></div>
+<div class="errorExplanation">Information coming soon.</div>
 
 <?php
   $updated = filemtime( $_SERVER["SCRIPT_FILENAME"] );
